8b3a26e959834ca89ef6dd69e04b5ee62a7d7161
Gołąb Sebastian <sebastian.golab@k5054macbook.local>
2025-05-29 18:58:04 +0200

    Add tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
+import "./styles/global.css";
+
 function App() {
-  return <h1>Hello Pigeon Components</h1>
+  return (
+    <h1 className="text-3xl font-bold underline">Hello Pigeon Components</h1>
+  );
 }
 
-export default App
+export default App;
